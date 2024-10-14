8bcae5fd17517c7aa48e642346dc4088784b009b
Gasco031103 <eesg031103@gmail.com>
2024-10-14 15:54:20 -0600

    Pruebas-Edgardo

--- web-site/run.py
+++ web-site/run.py
@@ -1,4 +1,14 @@
-from app import create_app
+import os
+from flask import Flask
+from app import routes
+
+def create_app():
+    app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'))
+
+    # Importa las rutas
+    app.register_blueprint(routes.main)
+
+    return app
 
 app = create_app()
 
