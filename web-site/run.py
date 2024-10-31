0603f9f6146cc7ce3ef2b552f1ae8113cd67deb7
EthanZash <ethan.zashuvath4939@alumnos.udg.mx>
2024-10-30 22:22:29 -0600

    Cambios en index, style, run y scanner

--- web-site/run.py
+++ web-site/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv('FLASK_DEBUG', '1') == '1'  # Usa '1' para activar depuración
+    app.run(debug=debug_mode)
