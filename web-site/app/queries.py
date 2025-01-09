6f2802b7dd31a076b55aa45de35b01f7cf01c80c
EthanZash <ethan.zashuvath4939@alumnos.udg.mx>
2025-01-08 22:30:08 -0600

    08/01/2025

---
+++ web-site/app/queries.py
@@ -0,0 +1,71 @@
+from .database import get_db_connection, release_db_connection
+
+def get_table_data(section):
+    """
+    Realiza una consulta para obtener los datos de una tabla específica.
+    """
+    connection = None
+
+    queries = {
+    'categories_resources': 
+    """
+    SELECT id AS "ID", name_category AS "Categoría"
+    FROM categories_resources
+    """,
+    'catalogue_resources': 
+    """
+    SELECT res.id AS "ID", res.name_resource AS "Recurso", res.description AS "Descripción",
+    CONCAT('$', TO_CHAR(res.price, 'FM999,999,999.00')) AS "Precio",
+    cat.name_category AS "Categoría", sup.name_supplier AS "Proveedor"
+    FROM resources res
+    INNER JOIN categories_resources cat ON res.id_category = cat.id
+    INNER JOIN supplier sup ON res.id_supplier = sup.id
+    """,
+    'supplier': 
+    """
+    SELECT id AS "ID", name_supplier AS "Proveedor", address AS "Dirección",
+    lada AS "Lada", phone AS "Teléfono", email AS "Email", delivery_time AS "Tiempo de distribución (Días)"
+    FROM supplier
+    """,
+    'categories_products': 
+    """
+    SELECT id AS "ID", name_category AS "Categoría" FROM categories_products
+    """,
+    'catalogue_products': 
+    """
+    SELECT pr.id AS "ID", pr.name_product AS "Producto", pr.description AS "Descripción",
+    CONCAT('$', TO_CHAR(pr.price, 'FM999,999,999.00')) AS "Precio",
+    cat.name_category AS "Categoría"  FROM products pr
+    INNER JOIN categories_products cat ON pr.id_category = cat.id
+    """,
+    'factories': 
+    """
+    SELECT id AS "ID", name AS "Fábrica", cod_postal AS "Código Postal",
+    address AS "Dirección", country AS "País", state AS "Estado"
+    FROM factories
+    """,
+    'equipment': 
+    """
+    SELECT equ.id AS "ID", fac.name AS "Fábrica", pr.name_product AS "Producto Producido",
+    equ.name AS "Máquina/Equipo", CONCAT(TO_CHAR(equ.min_prod, 'FM999,999,999'), ' ', equ.unit_measure) AS "Mínimo de Producción",
+    CONCAT(TO_CHAR(equ.time, 'FM999,999,999'), ' ', equ.unit_time) AS "Tiempo Necesario de Producción"
+    FROM equipment equ
+    INNER JOIN factories fac ON equ.id_factory = fac.id
+    INNER JOIN products pr ON equ.id_product = pr.id
+    """
+    }
+
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        query = queries.get(section)
+        cursor.execute(query)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        cursor.close()
+        return columns, data
+    except Exception as e:
+        raise Exception(f"Error al consultar la tabla: {e}")
+    finally:
+        if connection:
+            release_db_connection(connection)
--- web-site/app/routes.py
+++ web-site/app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .queries import get_table_data
 
 main = Blueprint('main', __name__)
 
@@ -6,6 +7,97 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+<<<<<<< Updated upstream
 @main.route('/about')
 def about():
     return render_template('about.html')
+=======
+@main.route('/config')
+def config():
+  return render_template('config.html')
+
+@main.route('/config-parameters/<string:section>')
+def config_parameters(section):
+  titles = {
+    'categories_resources': 'Categorías de Recursos',
+    'catalogue_resources': 'Catálogo de recursos',
+    'supplier': 'Proveedores',
+    'categories_products': 'Categorías de Productos',
+    'catalogue_products': 'Catálogo de Productos',
+    'factories': 'Fábricas',
+    'equipment': 'Equipo/Maquinaria'
+  }
+  title_section = titles.get(section, 'Gestión General')
+
+  tables = {
+    'categories_resources': 'categories_resources',
+    'catalogue_resources': 'resources',
+    'supplier': 'supplier',
+    'categories_products': 'categories_products',
+    'catalogue_products': 'products',
+    'factories': 'factories',
+    'equipment': 'equipment'
+  }
+  table = tables.get(section)
+
+  fields_configuration = {
+    'categories_resources': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'catalogue_resources': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+    ],
+    'supplier': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+    ],
+    'categories_products': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'catalogue_products': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'factories':[
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'equipment':[
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ]
+  }
+  fields = fields_configuration.get(section, [])
+
+  if not table:
+    return "Sección no válida", 404
+
+  try:
+    columns, data = get_table_data(section)
+  except Exception as e:
+    return f"Error al obtener los datos: {e}", 500
+  
+  return render_template(
+    'config-parameters.html',
+    section=section,
+    title_section=title_section,
+    columns=columns,
+    data=data,
+    fields=fields
+  )
+>>>>>>> Stashed changes
