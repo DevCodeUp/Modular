3eb51dfab800d71524ac324536d7d00cad3a73c8
Uli0883 <156877334+Uli0883@users.noreply.github.com>
2025-01-11 21:22:28 -0600

    Location

--- web-site/app/routes.py
+++ web-site/app/routes.py
@@ -1,11 +1,97 @@
 from flask import Blueprint, render_template
+from .queries import get_table_data
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
-@main.route('/about')
-def about():
-    return render_template('about.html')
+@main.route('/config')
+def config():
+  return render_template('config.html')
+
+@main.route('/config-parameters/<string:section>')
+def config_parameters(section):
+  titles = {
+    'categories_resources': 'Categorías de Recursos',
+    'catalogue_resources': 'Catálogo de recursos',
+    'supplier': 'Proveedores',
+    'categories_products': 'Categorías de Productos',
+    'catalogue_products': 'Catálogo de Productos',
+    'factories': 'Fábricas',
+    'equipment': 'Equipo/Maquinaria'
+  }
+  title_section = titles.get(section, 'Gestión General')
+
+  tables = {
+    'categories_resources': 'categories_resources',
+    'catalogue_resources': 'resources',
+    'supplier': 'supplier',
+    'categories_products': 'categories_products',
+    'catalogue_products': 'products',
+    'factories': 'factories',
+    'equipment': 'equipment'
+  }
+  table = tables.get(section)
+
+  fields_configuration = {
+    'categories_resources': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'catalogue_resources': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+    ],
+    'supplier': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+    ],
+    'categories_products': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'catalogue_products': [
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'factories':[
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ],
+    'equipment':[
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'},
+      {'name': 'name_category', 'type': 'text', 'label': 'Categoría'}
+    ]
+  }
+  fields = fields_configuration.get(section, [])
+
+  if not table:
+    return "Sección no válida", 404
+
+  try:
+    columns, data = get_table_data(section)
+  except Exception as e:
+    return f"Error al obtener los datos: {e}", 500
+  
+  return render_template(
+    'config-parameters.html',
+    section=section,
+    title_section=title_section,
+    columns=columns,
+    data=data,
+    fields=fields
+  )
--- web-site/run.py
+++ web-site/run.py
@@ -1,6 +1,19 @@
-from app import create_app
-
+import os
+from flask import Flask
+from app import routes  # Asegúrate de que routes está correctamente importado
+ 
+def create_app():
+  # Usa la ruta relativa basada en el archivo actual
+  app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+    
+  # Registra las rutas del blueprint (asegúrate de que 'routes.main' es un blueprint)
+  app.register_blueprint(routes.main)
+    
+  return app
+ 
+# Crea la aplicación
 app = create_app()
-
+ 
+# Ejecuta la aplicación si este archivo es el principal
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
